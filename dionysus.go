1c49689ad153eda3cb0f0dcb3731eea6cfe76a43
Fred Mitchell <fred.mitchell@gmx.de>
2019-07-26 06:07:45 +0200

    :art: point update

--- dionysus.go
+++ dionysus.go
@@ -1,23 +1,23 @@
 package main
 
 import (
-	"fmt"
 	"os"
+	"fmt"
 	
-	"github.com/karrick/godirwalk"
 	"github.com/flajann2/dionysus/minetype"
+	"github.com/karrick/godirwalk"
 )
 
 func main() {
 	dirname := "/grohnde/torrent"
 	err := godirwalk.Walk(dirname, &godirwalk.Options{
 		Callback: func(path string, de *godirwalk.Dirent) error {
-			if de.IsRegular(){
+			if de.IsRegular() {
 				ftype := minetype.Minetype(path)
 				fmt.Printf("%s %s %s\n",
 					de.ModeType(), path, ftype)
 			}
-			
+
 			return nil
 		},
 	})
