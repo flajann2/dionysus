61c3723058b7d130baed54d8f11f01e7657cf7e2
Fred Mitchell <fred.mitchell@gmx.de>
2019-07-26 06:41:56 +0200

    :art: adding channels

--- dionysus.go
+++ dionysus.go
@@ -6,18 +6,29 @@ import (
 	
 	"github.com/flajann2/dionysus/minetype"
 	"github.com/karrick/godirwalk"
+	"github.com/h2non/filetype/types"
 )
+
+type metadata struct {
+	path string
+	minetype types.Type
+	modetype os.FileMode
+}
 
 func main() {
 	dirname := "/grohnde/torrent"
+	ch := make(chan metadata, 100000)
+	
 	err := godirwalk.Walk(dirname, &godirwalk.Options{
 		Callback: func(path string, de *godirwalk.Dirent) error {
 			if de.IsRegular() {
-				ftype := minetype.Minetype(path)
+				m := metadata{path,
+					minetype.Minetype(path),
+					de.ModeType()}
+				
 				fmt.Printf("%s %s %s\n",
-					de.ModeType(), path, ftype)
+					m.modetype, m.path, m.minetype)
 			}
-
 			return nil
 		},
 	})
